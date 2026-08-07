f6a16953830970d88b872728d386fc109a007587
sujithkumar0017 <112610150+sujithkumar0017@users.noreply.github.com>
2026-08-07 14:50:59 +0530

    chore: configure playwright test execution

--- playwright.config.ts
+++ playwright.config.ts
@@ -23,13 +23,21 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
+  timeout: 30_000,
+  expect: {
+    timeout: 5_000,
+  },
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    /* Base URL to use in actions like `await page.goto('')`. */
-    // baseURL: 'http://localhost:3000',
+    baseURL: 'https://practicesoftwaretesting.com',
 
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    screenshot: 'only-on-failure',   //See failure state
+
+    video: 'retain-on-failure',    //Review execution leading to failure
+
+    trace: 'on-first-retry',      //Deep Playwright debugging
+
+    // headless: false,
   },
 
   /* Configure projects for major browsers */
